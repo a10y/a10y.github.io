d1c26d5eb9736e5794f18a25f132ccc38340312a
Andrew Duffy <aduffy@palantir.com>
2018-11-25 12:56:22 -0500

    pieces

--- setup-hooks.sh
+++ setup-hooks.sh
@@ -2,12 +2,13 @@
 
 PRE_PUSH_HOOK='.git/hooks/pre-push'
 
-if [[ ! -f ${PRE_PUSH_HOOK} ]]; then
-    cat<<EOF >${PRE_PUSH_HOOK}
+cat<<EOF >${PRE_PUSH_HOOK}
 # Re-deploy blog when pushing changes
 make deploy
 git add public/
-git commit -m 'Point public/ to latest deployed hash'
-EOF
-    chmod +x ${PRE_PUSH_HOOK}
+git status -s | grep -E '^A'
+if [[ $? -eq 0 ]]; then
+    git commit -m 'Point public/ to latest deployed hash'
 fi
+EOF
+chmod +x ${PRE_PUSH_HOOK}
